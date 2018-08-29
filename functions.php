34101cd0de195aedd0ffa7ab0d6ad2af2660d3e2
AndrewGM <andrewalangm@gmail.com>
2018-08-29 13:59:15 -0500

    Agregando Font awesome
    Se agregan los archivos a fonts
    Y se agrega el css como cualquier otro

--- functions.php
+++ functions.php
@@ -10,9 +10,11 @@
     function lapizzeria_styles(){
         //Registro de hojas de estilo
         wp_register_style('normalize',get_template_directory_uri() . '/css/normalize.css',array(), '8.0.0');
+        wp_register_style('fontawesome',get_template_directory_uri() . '/css/font-awesome.min.css',array('normalize'), '4.7.0');
         wp_register_style('mis_estilos',get_template_directory_uri() . '/style.css',array('normalize'), '1.0');
         //Encolando los estilos
         wp_enqueue_style('normalize');
+        wp_enqueue_style('fontawesome');
         wp_enqueue_style('mis_estilos');
     }
 
