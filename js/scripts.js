13b09389e023dbb4a07f87ed995b57b6cd6ba0d2
AndrewGM <andrewalangm@gmail.com>
2018-08-30 13:03:01 -0500

    Agregando funcionalidad menu mobil
    -por medio de JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
 $ = jQuery.noConflict();
 $(document).ready(function(){
-    alert("Documento listo");
+
+    // accion menu mobil
+    $(".mobile-menu a").click(function(){
+       $("nav.menu-sitio").toggle("slow");
+    });
+
+    // reaccionando al cambio de tamaño de pantalla
+    var breakpoint = 768;
+    $(window).resize(function(){
+        if($(document).width() >= breakpoint){
+            $("nav.menu-sitio").show();
+        }else{
+            $("nav.menu-sitio").hide();
+        }
+    });
+    
 });
