0024a565daaeb842edc1515084f102542cf4bbfc
AndrewGM <andrewalangm@gmail.com>
2018-08-31 23:12:40 -0500

    Agregando especialidades
    - Custom Page
    - Custom post field

--- functions.php
+++ functions.php
@@ -43,10 +43,54 @@
         add_image_size("nosotros",437 , 291, true);
     }
 
+    /**
+     * Agrega un custom post field, en el menu del wp admin
+     */
+    function lapizzeria_especialidades() {
+        $labels = array(
+            'name'               => _x( 'Pizzas', 'lapizzeria' ),
+            'singular_name'      => _x( 'Pizzas', 'post type singular name', 'lapizzeria' ),
+            'menu_name'          => _x( 'Pizzas', 'admin menu', 'lapizzeria' ),
+            'name_admin_bar'     => _x( 'Pizzas', 'add new on admin bar', 'lapizzeria' ),
+            'add_new'            => _x( 'Add New', 'book', 'lapizzeria' ),
+            'add_new_item'       => __( 'Add New Pizza', 'lapizzeria' ),
+            'new_item'           => __( 'New Pizzas', 'lapizzeria' ),
+            'edit_item'          => __( 'Edit Pizzas', 'lapizzeria' ),
+            'view_item'          => __( 'View Pizzas', 'lapizzeria' ),
+            'all_items'          => __( 'All Pizzas', 'lapizzeria' ),
+            'search_items'       => __( 'Search Pizzas', 'lapizzeria' ),
+            'parent_item_colon'  => __( 'Parent Pizzas:', 'lapizzeria' ),
+            'not_found'          => __( 'No Pizzases found.', 'lapizzeria' ),
+            'not_found_in_trash' => __( 'No Pizzases found in Trash.', 'lapizzeria' )
+        );
+    
+        $args = array(
+            'labels'             => $labels,
+        'description'        => __( 'Description.', 'lapizzeria' ),
+            'public'             => true,
+            'publicly_queryable' => true,
+            'show_ui'            => true,
+            'show_in_menu'       => true,
+            'query_var'          => true,
+            'rewrite'            => array( 'slug' => 'especialidades' ),
+            'capability_type'    => 'post',
+            'has_archive'        => true,
+            'hierarchical'       => false,
+            'menu_position'      => 6,
+            'supports'           => array( 'title', 'editor', 'thumbnail' ),
+        'taxonomies'          => array( 'category' ),
+        );
+    
+        register_post_type( 'especialidades', $args );
+    }
+
+
+
     /*************************
     ******** ACCIONES ********
     *************************/
     add_action('wp_enqueue_scripts','lapizzeria_styles');
     add_action('init','lapizzeria_menus');
     add_action('after_setup_theme','lapizzeria_setup');
+    add_action( 'init', 'lapizzeria_especialidades' );
 ?>
---
+++ page-especialidades.php
@@ -0,0 +1,29 @@
+<?php
+/*
+* Template Name: Especialidades
+*/
+    get_header(); 
+?>
+    <!-- LOOP DE WORDPRESS -->
+    <h1>Especialidades</h1>
+    <?php
+        while(have_posts()){
+    ?>  
+        <?php the_post(); ?>
+        <div class="hero" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>);">
+            <div class="contenido-hero">
+                <div class="texto-hero">
+                    <?php the_title("<h1>" , "</h1>"); ?>
+                </div>
+            </div>
+        </div>
+
+        <div class="principal contenedor">
+            <main class="texto-centrado contenido-paginas">
+                <?php the_content(); ?>
+            </main>
+        </div>
+
+    <?php } ?>
+    <!-- /LOOP WORDPRESS -->
+<?php get_footer(); ?>
