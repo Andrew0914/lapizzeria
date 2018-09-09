f4728950067dc736702848a30e5fbf0599cd19a8
AndrewGM <andrewalangm@gmail.com>
2018-09-08 19:59:52 -0500

    Imprimiendo los custom post con query manual
    WP_Query

--- page-especialidades.php
+++ page-especialidades.php
@@ -5,7 +5,6 @@
     get_header(); 
 ?>
     <!-- LOOP DE WORDPRESS -->
-    <h1>Especialidades</h1>
     <?php
         while(have_posts()){
     ?>  
@@ -26,4 +25,25 @@
 
     <?php } ?>
     <!-- /LOOP WORDPRESS -->
+    <!--LOOP PARA LA SECCION DE ESPECIALIDADES -->
+    <?php
+        $args = [
+            'post_type'=> 'especialidades',
+            'post_per_page'=> -1,
+            'orderby'=> 'title',
+            'order'=> 'ASC',
+            'category_name'=>'pizzas'
+        ];
+
+        $pizzas = new WP_Query($args);
+        while($pizzas->have_posts()){
+            $pizzas->the_post();
+    ?>
+        <ul>
+            <li>
+                <?php the_title();?>
+            </li>
+        </ul>
+    <?php } wp_reset_postdata(); ?>
+    <!--/LOOP PARA LA SECCION DE ESPECIALIDADES -->
 <?php get_footer(); ?>
