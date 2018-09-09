afb0654558a0ec052322cac5ffe3de6497c9c5ad
AndrewGM <andrewalangm@gmail.com>
2018-09-08 20:52:45 -0500

    Especialidades, despliegue, estilos grid
    Construyendo un grid de otra manera con css

--- page-especialidades.php
+++ page-especialidades.php
@@ -25,25 +25,60 @@
 
     <?php } ?>
     <!-- /LOOP WORDPRESS -->
-    <!--LOOP PARA LA SECCION DE ESPECIALIDADES -->
-    <?php
-        $args = [
-            'post_type'=> 'especialidades',
-            'post_per_page'=> -1,
-            'orderby'=> 'title',
-            'order'=> 'ASC',
-            'category_name'=>'pizzas'
-        ];
+    <!--GRID  ESPECIALIDADES PIZZAS-->
+    <div class="nuestras-especialidades contenedor">
+        <h3 class="texto-rojo">Pizzas</h3>
+        <div class="contenedor-grid">
+            <?php
+                $args = [
+                    'post_type'=> 'especialidades',
+                    'post_per_page'=> -1,
+                    'orderby'=> 'title',
+                    'order'=> 'ASC',
+                    'category_name'=>'pizzas'
+                ];
 
-        $pizzas = new WP_Query($args);
-        while($pizzas->have_posts()){
-            $pizzas->the_post();
-    ?>
-        <ul>
-            <li>
-                <?php the_title();?>
-            </li>
-        </ul>
-    <?php } wp_reset_postdata(); ?>
-    <!--/LOOP PARA LA SECCION DE ESPECIALIDADES -->
+                $pizzas = new WP_Query($args);
+                while($pizzas->have_posts()){
+                    $pizzas->the_post();
+            ?>
+                <div class="columnas2-4">
+                    <?php the_post_thumbnail('especialidades'); ?>
+                    <div class="texto-especialidad">
+                        <h4> <?php the_title(); ?> <span>$ <?php the_field('precio'); ?> </span> </h4>
+                        <?php the_content(); ?>
+                    </div>
+                </div>
+            <?php } wp_reset_postdata(); ?>
+        </div>
+    </div>
+    <!--/GRID ESPECIALIDADES PIZZAS-->
+    <!-- GRID ESPECIALIDADES OTROS-->
+    <div class="nuestras-especialidades contenedor">
+        <h3 class="texto-rojo">Otros</h3>
+        <div class="contenedor-grid">
+            <?php
+                $args = [
+                    'post_type'=> 'especialidades',
+                    'post_per_page'=> -1,
+                    'orderby'=> 'title',
+                    'order'=> 'ASC',
+                    'category_name'=>'otros'
+                ];
+
+                $otros = new WP_Query($args);
+                while($otros->have_posts()){
+                    $otros->the_post();
+            ?>
+                <div class="columnas2-4">
+                    <?php the_post_thumbnail('especialidades'); ?>
+                    <div class="texto-especialidad">
+                        <h4> <?php the_title(); ?> <span>$ <?php the_field('precio'); ?> </span> </h4>
+                        <?php the_content(); ?>
+                    </div>
+                </div>
+            <?php } wp_reset_postdata(); ?>
+        </div>
+    </div>
+    <!-- /GRID ESPECIALIDADES OTROS-->
 <?php get_footer(); ?>
