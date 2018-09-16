e072fb3bd719ca993d32187f0fb7ef08e079e5c7
AndrewGM <andrewalangm@gmail.com>
2018-09-15 21:05:09 -0500

    Colocando una imagen destacada a todo el blog
    Obteniendo la imagen con
    -get_post_thumbnail_id
    -wp_get_attachment_image_src
    Obteniendo la pagina para blogs con
    -get_option

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
 <?php get_header(); ?>
-    <h1>Hola desde index.php</h1>
+
+    <?php
+        $pagina_blog = get_option('page_for_posts');
+        $image_id = get_post_thumbnail_id($pagina_blog);
+        $image = wp_get_attachment_image_src($image_id,'full');
+    ?>
+    <div class="hero" style="background-image: url(<?php echo $image[0]; ?>);">
+        <div class="contenido-hero">
+            <div class="texto-hero">
+                <h1><?php echo get_the_title($pagina_blog); ?></h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="principal contenedor">
+        <main class="texto-centrado contenido-paginas">
+            <!-- LOOP DE WORDPRESS BLOG -->
+            <?php
+                while(have_posts()){
+                    the_post();
+                    the_title();
+            ?>     
+
+            <?php } ?>
+            <!-- /LOOP WORDPRESS BLOG -->
+        </main>
+    </div>
+
+    
 <?php get_footer(); ?>
