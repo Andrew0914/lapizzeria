405a1836a448008efcd64817714e074c6038f92f
AndrewGM <andrewalangm@gmail.com>
2018-09-16 00:26:32 -0500

    Loop del blog
    Funciones para imprimier diversos datos insertados en las entradas

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php get_header(); ?>
 
     <?php
+        // obtiene la pagina para entradas
         $pagina_blog = get_option('page_for_posts');
+        // obtiene el id de la imagen destacada
         $image_id = get_post_thumbnail_id($pagina_blog);
+        // obtiene un arreglo de la imagen del id, el index 0 es el url
         $image = wp_get_attachment_image_src($image_id,'full');
     ?>
     <div class="hero" style="background-image: url(<?php echo $image[0]; ?>);">
@@ -17,11 +20,36 @@
         <main class="texto-centrado contenido-paginas">
             <!-- LOOP DE WORDPRESS BLOG -->
             <?php
-                while(have_posts()){
-                    the_post();
-                    the_title();
+                while(have_posts()){ the_post();
             ?>     
-
+            <article class="entrada-blog">
+            <!-- IMAGEN DESTACADA -->
+            <?php
+                the_post_thumbnail("especialidades");
+            ?>
+                <header class="informacion-entrada clear">
+                    <!-- FECHA DEL POST-->
+                    <div class="fecha">
+                        <time>
+                            <?php the_time('d'); ?>
+                            <span><?php the_time('M'); ?></span>
+                        </time>
+                    </div>
+                    <!--TITULO Y AUTOR DEL POST-->
+                    <div class="titulo-entrada">
+                        <h2> <?php the_title(); ?> </h2>
+                        <p class="autor">
+                            <i class="fa fa-user" arias-hidden="true"> <?php the_author(); ?> </i>
+                        </p>
+                    </div>
+                </header>
+                <!-- CONTENIDO DE LA ENTRADA -->
+                <div class="contenido-entrada">
+                    <!-- PREVIEW DEL CONTENIDO-->
+                    <?php the_excerpt(); ?>
+                    <a href="<?php the_permalink(); ?>" class=boton rojo"">Leer más</a>
+                </div>
+            </article>
             <?php } ?>
             <!-- /LOOP WORDPRESS BLOG -->
         </main>
