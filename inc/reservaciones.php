a87cdb554c3d701a570ecd7e391de9d9b871c27f
AndrewGM <andrewalangm@gmail.com>
2018-09-18 14:35:29 -0500

    Sanitizando datos y seguridad de form
    Funciones sanitize de WP

--- inc/reservaciones.php
+++ inc/reservaciones.php
@@ -9,7 +9,11 @@
       */
     function lapizzeria_guardar(){
         if(isset($_POST['enviar']) && $_POST['oculto']== 1){
-            
+            $nombre = sanitize_text_field($_POST['nombre']);
+            $fecha = sanitize_text_field($_POST['fecha']);
+            $email = sanitize_email($_POST['email']);
+            $telefono = sanitize_text_field($_POST['telefono']);
+            $mensaje = sanitize_text_field($_POST['mensaje']);
         }
     }
 
