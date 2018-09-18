72424c9001aceb94df9d1f59f8e7166f0e24ec5e
AndrewGM <andrewalangm@gmail.com>
2018-09-18 14:53:20 -0500

    Agregando redireccion despues de guardar
    Evita la recarga de datos

--- inc/reservaciones.php
+++ inc/reservaciones.php
@@ -11,14 +11,15 @@
         
         global $wpdb;
         $tabla = $wpdb->prefix . "reservaciones";
-
+        // validamos el envio
         if(isset($_POST['enviar']) && $_POST['oculto']== 1){
+            //sanitizamos los campos
             $nombre = sanitize_text_field($_POST['nombre']);
             $fecha = sanitize_text_field($_POST['fecha']);
             $email = sanitize_email($_POST['email']);
             $telefono = sanitize_text_field($_POST['telefono']);
             $mensaje = sanitize_text_field($_POST['mensaje']);
-
+            // generamos los datos de insercion
             $datos = array(
                 "nombre" => $nombre,
                 "fecha" => $fecha,
@@ -26,10 +27,15 @@
                 "telefono" => $telefono,
                 "mensaje"=> $mensaje
             );
-            
+            // un formato por cada campo de insercion
             $formato = array('%s','%s','%s','%s','%s');
-
+            //insertamos
             $wpdb->insert($tabla,$datos,$formato);
+            // obtenemos la pagina de redireccion
+            $page = get_page_by_title("Gracias por su Reserva");
+            // redirigimos
+            wp_redirect(get_permalink($page->ID));
+            exit();
         }
     }
 
