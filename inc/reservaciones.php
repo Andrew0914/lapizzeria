21069c97d99b2849de071b8b327d0bfc93b8d1e1
AndrewGM <andrewalangm@gmail.com>
2018-09-18 14:48:12 -0500

    Haciendo la insersion a la base de datos

--- inc/reservaciones.php
+++ inc/reservaciones.php
@@ -8,12 +8,28 @@
       * Alamacena los datos del formulario de reservaciones
       */
     function lapizzeria_guardar(){
+        
+        global $wpdb;
+        $tabla = $wpdb->prefix . "reservaciones";
+
         if(isset($_POST['enviar']) && $_POST['oculto']== 1){
             $nombre = sanitize_text_field($_POST['nombre']);
             $fecha = sanitize_text_field($_POST['fecha']);
             $email = sanitize_email($_POST['email']);
             $telefono = sanitize_text_field($_POST['telefono']);
             $mensaje = sanitize_text_field($_POST['mensaje']);
+
+            $datos = array(
+                "nombre" => $nombre,
+                "fecha" => $fecha,
+                "correo" => $email,
+                "telefono" => $telefono,
+                "mensaje"=> $mensaje
+            );
+            
+            $formato = array('%s','%s','%s','%s','%s');
+
+            $wpdb->insert($tabla,$datos,$formato);
         }
     }
 
