72e8dfbbeadd1ccdef77adf2c44ca0ba4d60cd77
AndrewGM <andrewalangm@gmail.com>
2018-09-22 19:50:12 -0500

    Imprimiendo las reservaciones

--- inc/opciones.php
+++ inc/opciones.php
@@ -9,8 +9,34 @@
         add_submenu_page("lapizzeria_ajustes", "Reservaciones", "Reservaciones", "administrator", "lapizzeria_reservaciones", "lapizzeria_reservaciones");
     }
 
-    function lapizzeria_reservaciones(){}
-        
+    function lapizzeria_reservaciones(){
+        echo "<div class='wrap'>";
+            echo "<h1>Reservaciones</h1>";
+            echo "<table class='wp-list-table widefat striped'>";
+                echo "<thead>";
+                    echo "<tr>";
+                        echo "<th class='manage-column'>ID</th> <th class='manage-column'>Nombre</th> <th class='manage-column'>Fecha</th> <th class='manage-column'>Correo</th> <th class='manage-column'>Telefono</th> <th class='manage-column'>Mensaje</th>";
+                    echo "</tr>";
+                echo "</thead>";
+                echo "<tbody>";
+                    global $wpdb;
+                    $reservaciones_table = $wpdb->prefix.'reservaciones';
+                    $registros = $wpdb->get_results("SELECT * FROM $reservaciones_table",ARRAY_A);
+                    foreach($registros as $row){
+                        echo "<tr>";
+                            echo "<td>" . $row['id'] . "</td>";
+                            echo "<td>" . $row['nombre'] . "</td>";
+                            echo "<td>" . $row['fecha'] . "</td>";
+                            echo "<td>" . $row['correo'] . "</td>";
+                            echo "<td>" . $row['telefono'] . "</td>";
+                            echo "<td>" . $row['mensaje'] . "</td>";
+                        echo "</tr>";
+                    }
+                echo "</tbody>";
+            echo "</table>";
+        echo "</div>";
+    }
+
     /**
     * ACCIONES
     */
