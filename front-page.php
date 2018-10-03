be138e22e40988b6159bbe0f9030559a0886e664
AndrewGM <andrewalangm@gmail.com>
2018-10-03 09:10:42 -0500

    Utilizando front-page.php y agregando primeros contenidos

---
+++ front-page.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+    <!-- LOOP DE WORDPRESS -->
+    <?php
+        while(have_posts()){
+    ?>  
+        <?php the_post(); ?>
+        <div class="hero" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>);">
+            <div class="contenido-hero">
+                <div class="texto-hero">
+                    <h1><?php echo get_option('blogdescription'); ?></h1>
+                    <?php the_content(); ?>
+                    <?php $page = get_page_by_title("Sobre Nosotros");?>
+                    <a href="<?php echo get_permalink($page->ID); ?>" class="boton">Saber más</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="principal contenedor">
+            <main class="texto-centrado contenido-paginas">
+                
+            </main>
+        </div>
+
+    <?php } ?>
+    <!-- /LOOP WORDPRESS -->
+<?php get_footer(); ?>
